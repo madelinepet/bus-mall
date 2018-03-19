93798b09c02b4ba6cdecc9c3f1d446d34b3eeaa1
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-03-19 13:29:40 -0700

    Set up HTML and linked HTML with script. Set up a starting plan for the project in the form of comments in my app.js.

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+//define variables including an array to push all instances into. Include a counter to track how many clicks overall.
+
+//constructor function with a counter set to zero so that for each image, you have a different counter. Also include a counter for how many time each is displayed. Add content to the ul called "results"
+
+//prototypes with for loop to track clicks and views each time the new set of images displays.
+
+//new instances
+
+//access the element by ID from the DOM
+
+//add event listener to track when each image spot clicked, and when it is run callback function below.
+
+//callback function for when each of the three images clicked. Will include a random number generator. Needs to redisplay images when clicked. Needs images to be different each time.
+
+//call function to display on page load
+
+//display results to user
