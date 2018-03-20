4c6c30e546ed6801659490fae0b087267ed17a84
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-03-19 21:18:28 -0700

    My counters are all working and displaying in list form for all of my instances.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ Pic.clickableArea = document.getElementById('clickCount');
 //define variables including an array to push all instances into. Include a counter to track how many clicks overall.
 Pic.allPics = [];
 
+//need an array with only the current object in it.
 Pic.currentPic =[];
 
 //need a global variable to track how many votes have happened because unlike the display counter and the click counter, it is not specific to each instance
@@ -44,28 +45,12 @@ new Pic('img/usb.gif','usb');
 new Pic('img/water-can.jpg','water can');
 new Pic('img/wine-glass.jpg','wine glass');
 
-//if else to track clicks and views each time the new set of images displays. Limit number of clicks to 25
-//only display results here 
-function resultsRender(event) {
-  //if the vote counter is less than 25, keep displaying results, create new list item with the results, but display results only after the counter has reached 25
-  for(var i=0; i < Pic.allPics.length; i++){
-    if(event.target.alt === Pic.allPics[i].name){
-      Pic.allPics[i].voteCounter ++;
-    }
-  }
-  while(clickCounter < 5){
-    //do this only when clicked???????????????????????????????????????????????????????????????????
-    //Add content to the ul called "results" accessed at top, in the form of new li elements
-    var listElement = document.createElement('li');
-    listElement.textContent = (Pic.eventname + ' has ' + Pic.displayCounter + ' views and ' + Pic.clickCounter + ' votes.');
-    // //increment voteCounter
-    console.log('event.target', event.target.src);
-    clickCounter++;
-  }
-  if(clickCounter === 5){
-    //stop additional voting from happening
 
-    //append to list to display only when this is true
+//only use this function to display results, the counters are calculated below
+function resultsRender() {
+  for(var i=0; i < Pic.allPics.length; i++){
+    var listElement= document.createElement('li');
+    listElement.textContent = (Pic.allPics[i].name + ' has ' + Pic.allPics[i].displayCounter + ' views and ' + Pic.allPics[i].voteCounter + ' votes.');
     totalListElement.appendChild(listElement);
   }
 }
@@ -81,11 +66,25 @@ imgTwoElement.addEventListener('click', clickHandler);
 imgThreeElement.addEventListener('click', clickHandler);
 
 function clickHandler(event){
-  //clickcounter global++
-  //if event.target.id === img1, add one to the click counter of the current pics array at 0
-  //if clickcounter <=25, show results,  then set display to none
-  console.log(event.target.id);
-  randomPic();
+  clickCounter ++;
+  if (event.target.id === 'img1') {
+    //add one to the click counter of the current pics array at 0
+    Pic.currentPic[0].voteCounter++;
+  }
+  if(event.target.id === 'img2'){
+    Pic.currentPic[1].voteCounter++;
+  }
+  if(event.target.id === 'img3'){
+    Pic.currentPic[2].voteCounter++;
+  }
+  if (clickCounter >=25) {
+    resultsRender();
+  //then set display to none so user can no longer vote
+  }
+  else{
+    console.log(event.target.id);
+    randomPic();
+  }
 }
 //callback function for when each of the three images clicked. Will include a random number generator. Needs to redisplay images when clicked. Needs images to be different each time.
 function randomPic() {
@@ -124,7 +123,3 @@ function randomPic() {
 //call function to display on page load
 
 randomPic();
-// clickTracker();
-
-
-//issues: call clicktracker function, make the while loop only happen on click, stop voting from happening!
