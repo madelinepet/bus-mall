893d6ab966659ccdc157bcf6e1a524e451a483cf
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-03-20 16:18:48 -0700

    I fixed my user stories to match the format. I have a chart that displays in colors that I like and I fixed my randomization. I am working on my css and design.

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var picVotes = [];
 //need a global variable to track how many votes have happened because unlike the display counter and the click counter, it is not specific to each instance
 var clickCounter = 0;
 
-//access the ul given the ID "results" in my HTML
-var totalListElement = document.getElementById('results');
+//access the ul given the ID "results" in my HTML. Uncomment this, and resultsRender to get list back
+// var totalListElement = document.getElementById('results');
 
 //constructor function with a counter set to zero so that for each image, you have a different counter. Also include a counter for how many time each is displayed.
 function Pic(filePath, name){
@@ -55,14 +55,14 @@ new Pic('img/water-can.jpg','water can');
 new Pic('img/wine-glass.jpg','wine glass');
 
 
-//only use this function to display results, the counters are calculated below
-function resultsRender() {
-  for(var i=0; i < Pic.allPics.length; i++){
-    var listElement= document.createElement('li');
-    listElement.textContent = (Pic.allPics[i].name + ' has ' + Pic.allPics[i].displayCounter + ' views and ' + Pic.allPics[i].voteCounter + ' votes.');
-    totalListElement.appendChild(listElement);
-  }
-}
+//only use this function to display results, the counters are calculated below. uncomment this and function call to get list back
+// function resultsRender() {
+//   for(var i=0; i < Pic.allPics.length; i++){
+//     var listElement= document.createElement('li');
+//     listElement.textContent = (Pic.allPics[i].name + ' has ' + Pic.allPics[i].displayCounter + ' views and ' + Pic.allPics[i].voteCounter + ' votes.');
+//     totalListElement.appendChild(listElement);
+//   }
+// }
 //add event listener to section. Repplaces handler for each individual displayed image I had before.
 Pic.clickableArea.addEventListener('click', clickHandler);
 
@@ -88,7 +88,7 @@ function clickHandler(event){
     imgThreeElement.removeEventListener('click', clickHandler);
 
 
-    //display results as list. Uncomment to see list.
+    //display results as list. Uncomment this and resultsRender function to see list.
     // resultsRender();
 
     //update the votes per goat
@@ -114,7 +114,7 @@ function randomPic() {
   var randomIndex2 = Math.floor(Math.random()* Pic.allPics.length);
 
   var randomIndex3 = Math.floor(Math.random()* Pic.allPics.length);
-console.log('previous pics before while', Pic.previousPics);
+  console.log('previous pics before while', Pic.previousPics);
   //in here, in the || conditions, also include || for the three pics using .includes conditions for the Pic.currentPic array so that the next display does not have repeat images.
   while(randomIndex1===randomIndex2 || randomIndex1===randomIndex3 || randomIndex2===randomIndex3 || Pic.previousPics.includes(randomIndex1) || Pic.previousPics.includes(randomIndex2) || Pic.previousPics.includes(randomIndex3)){
     //console log here to see how often you hit duplicates
@@ -135,17 +135,18 @@ console.log('previous pics before while', Pic.previousPics);
   imgOneElement.alt = Pic.allPics[randomIndex1].name;
   Pic.allPics[randomIndex1].displayCounter++;
   Pic.currentPic.push(randomIndex1);
-  
+
   imgTwoElement.src = Pic.allPics[randomIndex2].filePath;
   imgTwoElement.alt = Pic.allPics[randomIndex2].name;
   Pic.allPics[randomIndex2].displayCounter++;
   Pic.currentPic.push(randomIndex2);
-  
+
   imgThreeElement.src = Pic.allPics[randomIndex3].filePath;
   imgThreeElement.alt = Pic.allPics[randomIndex3].name;
   Pic.allPics[randomIndex3].displayCounter++;
   Pic.currentPic.push(randomIndex3);
-  
+
+  //need to push into old pics array last because the first time it runs, need to pupulate the array so it can compare.
   Pic.previousPics.push.apply(Pic.previousPics, Pic.currentPic);
 }
 //call function to display on page load
--- user_stories.md
+++ user_stories.md
@@ -2,20 +2,20 @@
 
 ##Marketing Research Team
 ###As a marketing research team, we would like to create a website that tracks clicks so that we can rank the popularity of our potential products.
-###As a marketing research team, we would like to gather data before we launch our official invintory.
+###As a marketing research team, we would like to gather data before we launch our official invintory so that we can know more information about what customers might be interested in.
 ###As a marketing research team, we would like to provide a user with unique options to click so that we can weigh viewed versus clicked and analyze the data.
-###As a marketing research team, we would like an interactive website that makes our approach modern for a modern market.
+###As a marketing research team, we would like an interactive website so that our approach is presented in a modern way to a modern market.
 
 ##Developer
-###As a developer, I would like to create a website that tracks clicks to help the marketing research team gather data.
-###As a developer, I would like to create a website that uses all of my current knowledge in a new way to provide a user with an interactive experience.
-###As a developer, I would like to create a website according to the marketing research team's specifications.
-###As a developer, I would like to create a website that collects and displays information from my JS code.
-###As a developer, I would like to start out by planning my code before I start writing it.
+###As a developer, I would like to create a website that tracks clicks so that the marketing research team can gather data.
+###As a developer, I would like to create a website that uses all of my current knowledge so that I can provide a user with an interactive experience.
+###As a developer, I would like to create a website so that it matches the marketing research team's specifications.
+###As a developer, I would like to create a website that collects and displays information from my JS code so that I can solidify the JavaScript node techniques I have learned.
+###As a developer, I would like to start out by planning my code before I start writing it so that I can better think through the problem domain.
 
 
 ##Focus Group Participants 
-###As a focus group participant, I would like to help the marketing research team gather information for their product popularity.
-###As a focus group participant, I would like to have an expeerience that is interactive.
-###As a focus group participant, I would like to share my personal preferences.
-###As a focus group participant, I would like to be a part of a world that relies on technology.
+###As a focus group participant, I would like to help the marketing research team gather information for their product popularity so that they can launch their magazine.
+###As a focus group participant, I would like to have an expeerience that is interactive so that I enjoy the research process.
+###As a focus group participant, I would like to share my personal preferences so that I can help a small, local company.
+###As a focus group participant, I would like to participate in online reseach so that I am playing an active role in a world that relies on technology.
